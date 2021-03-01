d4f66587fece490d0dfb2401b462e949f063d6bf
Smarty18 <marty.esp18@gmail.com>
2021-03-01 16:35:17 +0100

    Mod_Martina

--- primoProgramma.py
+++ primoProgramma.py
@@ -1,12 +1,7 @@
-print("hello world")
+print("Hello world")
 
 print("ciao")
 #ok é comparso
 
 print('prova')
-<<<<<<< HEAD
 #aperto da terminale
-=======
-
-#commit quattro
->>>>>>> d2ffd70ee12d6970c28b14b70774ece096acdd11
